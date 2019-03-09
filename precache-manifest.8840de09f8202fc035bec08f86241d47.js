6417a5b242b3adab1806f4996f84f801cd2c7ef0
dhairyadwivedi <dhairiya96@gmail.com>
2019-03-09 14:21:51 +0530

    Updates

---
+++ precache-manifest.8840de09f8202fc035bec08f86241d47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "49f931aa27533430992d536ba14c2986",
+    "url": "/react-weather-app/static/media/background.49f931aa.svg"
+  },
+  {
+    "revision": "4c4cfa3beddf0241a8a8",
+    "url": "/react-weather-app/static/js/runtime~main.4c4cfa3b.js"
+  },
+  {
+    "revision": "a7b4701150daa8c86139",
+    "url": "/react-weather-app/static/js/main.a7b47011.chunk.js"
+  },
+  {
+    "revision": "d24e2b17fb101d1b58f6",
+    "url": "/react-weather-app/static/js/2.d24e2b17.chunk.js"
+  },
+  {
+    "revision": "a7b4701150daa8c86139",
+    "url": "/react-weather-app/static/css/main.00ff1d62.chunk.css"
+  },
+  {
+    "revision": "d24e2b17fb101d1b58f6",
+    "url": "/react-weather-app/static/css/2.6533b1bc.chunk.css"
+  },
+  {
+    "revision": "40bac52c3f0da6d760ac0eb0aa171818",
+    "url": "/react-weather-app/index.html"
+  }
+];
